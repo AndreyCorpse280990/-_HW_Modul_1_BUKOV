8cbb074c0032737bbb1aa53cbc39deddf15867c9
AndreyCorpse28090 <280990corpse@gmail.com>
2024-02-09 22:29:47 +0300

    2

--- Program.cs
+++ Program.cs
@@ -25,15 +25,15 @@ namespace C__HW_Modul_1_Buikov
 
             if (number >= 1 && number <= 100)
             {
-                if (number % 5 == 0 && number % 3 == 0)
+                if (number % 5 == 0 && number % 3 == 0) // Если кратно 5 и 3
                 {
                     Console.WriteLine("Fizz Buzz");
                 }
-                else if (number % 5 == 0)
+                else if (number % 5 == 0) // Если кратно 5
                 {
                     Console.WriteLine("Buzz");
                 }
-                else if (number % 3 == 0)
+                else if (number % 3 == 0) // Если кратно 3
                 {
                     Console.WriteLine("Fizz");
                 }
@@ -69,18 +69,57 @@ namespace C__HW_Modul_1_Buikov
         {
             /* ЗАДАНИЕ 3
             * Пользователь вводит с клавиатуры четыре цифры.
-           Необходимо создать число, содержащее эти цифры. Например, если с клавиатуры введено 1, 5, 7, 8 тогда нужно
-           сформировать число 1578.*/
+            * Необходимо создать число, содержащее эти цифры. Например,
+            * если с клавиатуры введено 1, 5, 7, 8 тогда нужно
+            * сформировать число 1578.*/
             Console.WriteLine("\nЗадание 3");
-            Console.WriteLine("введите четырёхзначный номер ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            int num3 = Convert.ToInt32(Console.ReadLine());
-            int num4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите четырёхзначный номер ");
+            int num1, num2, num3, num4;
+
+            do
+            {
+                Console.WriteLine("Введите первую цифру:");
+                int.TryParse(Console.ReadLine(), out num1);
+                if (num1 < 0 || num1 > 9)
+                {
+                    Console.WriteLine("Ошибка: число должно быть от 0 до 9.");
+                }
+            } while (num1 < 0 || num1 > 9);
+
+            do
+            {
+                Console.WriteLine("Введите вторую цифру:");
+                int.TryParse(Console.ReadLine(), out num2);
+                if (num2 < 0 || num2 > 9)
+                {
+                    Console.WriteLine("Ошибка: число должно быть от 0 до 9.");
+                }
+            } while (num2 < 0 || num2 > 9);
+
+            do
+            {
+                Console.WriteLine("Введите третью цифру:");
+                int.TryParse(Console.ReadLine(), out num3);
+                if (num3 < 0 || num3 > 9)
+                {
+                    Console.WriteLine("Ошибка: число должно быть от 0 до 9.");
+                }
+            } while (num3 < 0 || num3 > 9);
+
+            do
+            {
+                Console.WriteLine("Введите четвёртую цифру:");
+                int.TryParse(Console.ReadLine(), out num4);
+                if (num4 < 0 || num4 > 9)
+                {
+                    Console.WriteLine("Ошибка: число должно быть от 0 до 9.");
+                }
+            } while (num4 < 0 || num4 > 9);
+
             int result = num1 * 1000 + num2 * 100 + num3 * 10 + num4;
             Console.WriteLine($"Число, сформированное из введенных цифр: {result}");
-
         }
+
 
         static void Task4()
         {
@@ -97,12 +136,14 @@ namespace C__HW_Modul_1_Buikov
             Console.WriteLine("Введите шестизначное число:");
             int sixNum = Convert.ToInt32(Console.ReadLine());
 
+            // Проверка, что введенное число шестизначное
             if (sixNum >= 100000 && sixNum <= 999999)
             {
                 Console.WriteLine("Введите два номера разрядов для обмена (например, 1 и 2):");
                 int change1 = Convert.ToInt32(Console.ReadLine());
                 int change2 = Convert.ToInt32(Console.ReadLine());
 
+                // Проверка корректности номеров разрядов для обмена
                 if (change1 >= 1 && change1 <= 6 && change2 >= 1 && change2 <= 6)
                 {
                     // разбивка числа на цифры
@@ -150,7 +191,7 @@ namespace C__HW_Modul_1_Buikov
             Console.WriteLine("\nЗадание 5");
             Console.WriteLine("Введите дату, например 04.04.2024");
             string date = Console.ReadLine();
-            string[] dateSplit = date.Split('.');
+            string[] dateSplit = date.Split('.'); // разбивка на dd.MM.yyyy
 
             int day = Convert.ToInt32(dateSplit[0]);
             int month = Convert.ToInt32(dateSplit[1]);
@@ -182,7 +223,8 @@ namespace C__HW_Modul_1_Buikov
             }
 
             // Количество дней от начала года до даты. формула Зеллера
-            int daysSinceStartOfYear = (day + (13 * (month + 1)) / 5 + year % 100 + (year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
+            int daysSinceStartOfYear = (day + (13 * (month + 1)) / 5 + year % 100 + (year % 100) / 4 + (year / 100) 
+                / 4 + 5 * (year / 100)) % 7;
             string[] daysOfWeek = { "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
             string strDayOfWeek = daysOfWeek[daysSinceStartOfYear];
 
@@ -200,7 +242,7 @@ namespace C__HW_Modul_1_Buikov
              */
             Console.WriteLine("\nЗадание 6");
             Console.WriteLine("Введите температуру.");
-            double temperature = Convert.ToInt32(Console.ReadLine());
+            double temperature = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Выберите во что перевести(1 - F| 2 - C)");
             int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -249,7 +291,7 @@ namespace C__HW_Modul_1_Buikov
         }
         static void Main(string[] args)
         {
-            Task1();
+            Task7();
         }
     }
 }
